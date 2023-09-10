ecec39e43ba04139fb3bbdb230765567c3628399
piyadargan <140742672+piyadargan@users.noreply.github.com>
2023-09-10 16:46:46 -0400

    tele

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/BrainSTEMTeleOp.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.tele;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Tele Op")
+public class BrainSTEMTeleOp extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
