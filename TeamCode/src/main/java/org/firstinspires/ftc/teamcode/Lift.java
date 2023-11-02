42cf13a7c0436f81592ab4f92f646f8236be1891
piyadargan <140742672+piyadargan@users.noreply.github.com>
2023-11-01 20:44:15 -0400

    lift class
    beggingn of lift auto
    
    Co-Authored-By: seonigandhi <144625762+seonigandhi@users.noreply.github.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.util.Map;
+
+public class Lift {
+    private Telemetry telemetry;
+    private DcMotorEx LiftMotor;
+    private Map statemap;
+}
