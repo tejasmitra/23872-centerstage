78e2196438a46a796acebb6f4facc663462f8c6f
piyadargan <140742672+piyadargan@users.noreply.github.com>
2023-11-12 09:09:43 -0500

    Adjust sensitivity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/BrainSTEMTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/BrainSTEMTeleOp.java
@@ -36,10 +36,10 @@ public class BrainSTEMTeleOp extends LinearOpMode {
         while (opModeIsActive()) {
             drive.setDrivePowers(new PoseVelocity2d(
                     new Vector2d(
-                            -gamepad1.left_stick_y,
-                            -gamepad1.left_stick_x
+                            -gamepad1.left_stick_y * 0.2,
+                            -gamepad1.left_stick_x * 0.2
                     ),
-                    -gamepad1.right_stick_x
+                    -gamepad1.right_stick_x * 0.2
             ));
 
             drive.updatePoseEstimate();
